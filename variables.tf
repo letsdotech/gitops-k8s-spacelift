c55a1831bfd280cd0ab77c01abb0c89e3b6afe6c
Let's Do Tech <113388070+letsdotech@users.noreply.github.com>
2025-04-29 22:18:42 +0200

    fixes

--- variables.tf
+++ variables.tf
@@ -1,21 +1,5 @@
-variable "kube_config_path" {
-  description = "Path to the kubeconfig file"
-  type        = string
-  default     = "~/.kube/config"
-}
-
 variable "kubernetes_host" {
   description = "The hostname (in form of URI) of Kubernetes master"
-  type        = string
-}
-
-variable "kubernetes_client_certificate" {
-  description = "PEM-encoded client certificate for TLS authentication"
-  type        = string
-}
-
-variable "kubernetes_client_key" {
-  description = "PEM-encoded client certificate key for TLS authentication"
   type        = string
 }
 
